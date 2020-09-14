357f8e8480b57677d5aba9d64f65155ddfff672e
AmimoG <gilbertmatete6@gmail.com>
2020-09-14 08:39:08 +0300

    Add Files

--- app/request.py
+++ app/request.py
@@ -32,3 +32,39 @@ def get_source():
             source_results = process_results(source_results_list)
 
     return source_results
+
+def process_results(source_list):
+    '''
+    function to process results and transform them to a list of objects
+    Args:
+        source_list:dictionary cotaining source details
+    Returns:
+        source_results: A list of source objects
+    '''
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        if id:
+            source_object = Source(id,name,description,url)
+            source_results.append(source_object)
+
+    return source_results
+
+def article_source(id):
+    article_source_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id,api_key)
+    print(article_source_url)
+    with urllib.request.urlopen(article_source_url) as url:
+        article_source_data = url.read()
+        article_source_response = json.loads(article_source_data)
+
+        article_source_results = None
+
+        if article_source_response['articles']:
+            article_source_list = article_source_response['articles']
+            article_source_results = process_articles_results(article_source_list)
+
+
+    return article_source_results
